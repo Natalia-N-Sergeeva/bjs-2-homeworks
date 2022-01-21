51614dcd36a533b1f3231bd127417eb62e1013fe
Natalia <sergeeva-2055@mail.ru>
2022-01-21 23:34:16 +0400

    добавлена функция для решения квадратного уравнения

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,21 @@
+'use strict'
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
-}
+  let D = Math.pow(b, 2)-4*a*c;
+  let x = -b/(2*a);
+  let x1 = (-b + Math.sqrt(D) )/(2*a);
+  let x2 = (-b - Math.sqrt(D) )/(2*a);
+  if (D == 0) {
+   arr = [x]; 
+  } else if (D > 0) {
+    arr = [x1, x2];
+  } else {
+    arr = [];
+  }
+  return arr;
+}  
+
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
