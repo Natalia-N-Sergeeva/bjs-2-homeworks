cf3156f9cd0ae0128d3c99ef98b4609abbc03070
Natalia <sergeeva-2055@mail.ru>
2022-05-18 11:01:42 +0400

    objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -12,13 +12,14 @@ function Student(name, gender, age) {
 
 Student.prototype.setSubject = function (subjectName) {
   this.subject = subjectName;
-
 }
 
- student4.setSubject("math");
- console.log(student4.subject);
 
- Student.prototype.addMark = function (mark) {
+student4.setSubject("math");
+console.log(student4.subject);
+
+
+Student.prototype.addMark = function (mark) {
   if(this.marks === undefined){ 
      this.marks=[mark];
     } else {
@@ -26,28 +27,22 @@ Student.prototype.setSubject = function (subjectName) {
     }
 }
  
- Student.prototype.addMarks= function(mark1,mark2,mark3, ...anotherMarks){
-  if(this.marks === undefined){ 
-     this.marks=[mark1, mark2, mark3];
-     for (i=0; i<anotherMarks.length; i++){
-        this.marks.push(anotherMarks[i]);
-      }
-    }else {
-      this.marks.push(mark1,mark2,mark3);
-      for (i=0; i<anotherMarks.length; i++){
-        this.marks.push(anotherMarks[i]);
-      }
-    }
-  }
 
-  Student.prototype.getAverage = function () {
-  this.average = this.marks.reduce((acc, item, idx, arr) => {
+Student.prototype.addMarks= function(...anotherMarks){
+  this.marks=[];
+  this.marks.push(...anotherMarks);
+ }
+
+
+Student.prototype.getAverage = function () {
+  let average = this.marks.reduce((acc, item, idx, arr) => {
   if (idx === arr.length-1){
     return (acc+item)/arr.length;
     } else {
       return acc + item;
     }
   })
+  return average;
 }
 
 Student.prototype.exclude  = function (reason) {
@@ -62,8 +57,4 @@ Student.prototype.exclude  = function (reason) {
 
     
 
-student4.addMarks(1,2,3,4,5,6);
-student4.getAverage();
- console.log(student4.marks);
- console.log(student4.average)
 
